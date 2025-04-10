1fb266b5f902ea00b53bcccf211766aaded12311
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2025-04-10 18:47:01 +0500

    some changes addeed

--- app/main/aside/index.tsx
+++ app/main/aside/index.tsx
@@ -1,71 +1,72 @@
 import { RightOutlined } from "@ant-design/icons";
 import Link from "next/link";
-import React from "react";
 
 const Aside = () => {
   return (
-    <div className="sticky top-14 max-[1024px]:w-[2000px] max-[1024px]:!static">
-      <div className="max-[1024px]:overflow-scroll max-[1024px]:flex">
-        <Link
-          href={"/"}
-          className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
-          <p>🇺🇿 O&apos;zbekistonga yetkazish</p>
-          <RightOutlined className="max-[1024px]:!hidden" />
-        </Link>
-        <Link
-          href={"/"}
-          className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
-          <p>Go&apos;sht va Baliq</p>
-          <RightOutlined className="max-[1024px]:!hidden" />
-        </Link>
-        <Link
-          href={"/"}
-          className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
-          <p>Asosiy oziq-ovqatlar</p>
-          <RightOutlined className="max-[1024px]:!hidden" />
-        </Link>
-        <Link
-          href={"/"}
-          className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
-          <p>Asosiy oziq-ovqatlar</p>
-          <RightOutlined className="max-[1024px]:!hidden" />
-        </Link>
-        <Link
-          href={"/"}
-          className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
-          <p>Tayyor oziq-ovqatlar</p>
-          <RightOutlined className="max-[1024px]:!hidden" />
-        </Link>
-        <Link
-          href={"/"}
-          className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
-          <p>Ichimliklar</p>
-          <RightOutlined className="max-[1024px]:!hidden" />
-        </Link>
-        <Link
-          href={"/"}
-          className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
-          <p>Mobil telefon va kompyuter...</p>
-          <RightOutlined className="max-[1024px]:!hidden" />
-        </Link>
-        <Link
-          href={"/"}
-          className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
-          <p>Oilaviy do&apos;kon</p>
-          <RightOutlined className="max-[1024px]:!hidden" />
-        </Link>
-        <Link
-          href={"/"}
-          className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
-          <p>Kitoblar</p>
-          <RightOutlined className="max-[1024px]:!hidden" />
-        </Link>
-        <Link
-          href={"/"}
-          className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
-          <p>Shirinliklar</p>
-          <RightOutlined className="max-[1024px]:!hidden" />
-        </Link>
+    <div className="min-[1025px]:sticky min-[1025px]:top-14 max-[1024px]:relative max-[1024px]:top-0 w-full">
+      <div className="max-[1024px]:overflow-x-auto max-[1024px]:px-4 max-[1024px]:py-2 scrollbar-hide">
+        <div className="flex max-[1024px]:w-max max-[1024px]:flex-row flex-col">
+          <Link
+            href={"/"}
+            className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
+            <p>🇺🇿 O&apos;zbekistonga yetkazish</p>
+            <RightOutlined className="max-[1024px]:!hidden" />
+          </Link>
+          <Link
+            href={"/"}
+            className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
+            <p>Go&apos;sht va Baliq</p>
+            <RightOutlined className="max-[1024px]:!hidden" />
+          </Link>
+          <Link
+            href={"/"}
+            className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
+            <p>Asosiy oziq-ovqatlar</p>
+            <RightOutlined className="max-[1024px]:!hidden" />
+          </Link>
+          <Link
+            href={"/"}
+            className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
+            <p>Asosiy oziq-ovqatlar</p>
+            <RightOutlined className="max-[1024px]:!hidden" />
+          </Link>
+          <Link
+            href={"/"}
+            className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
+            <p>Tayyor oziq-ovqatlar</p>
+            <RightOutlined className="max-[1024px]:!hidden" />
+          </Link>
+          <Link
+            href={"/"}
+            className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
+            <p>Ichimliklar</p>
+            <RightOutlined className="max-[1024px]:!hidden" />
+          </Link>
+          <Link
+            href={"/"}
+            className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
+            <p>Mobil telefon va kompyuter...</p>
+            <RightOutlined className="max-[1024px]:!hidden" />
+          </Link>
+          <Link
+            href={"/"}
+            className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
+            <p>Oilaviy do&apos;kon</p>
+            <RightOutlined className="max-[1024px]:!hidden" />
+          </Link>
+          <Link
+            href={"/"}
+            className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
+            <p>Kitoblar</p>
+            <RightOutlined className="max-[1024px]:!hidden" />
+          </Link>
+          <Link
+            href={"/"}
+            className="flex items-center gap-6 py-3 justify-between pr-6 text-[14px]">
+            <p>Shirinliklar</p>
+            <RightOutlined className="max-[1024px]:!hidden" />
+          </Link>
+        </div>
       </div>
     </div>
   );
--- app/main/index.tsx
+++ app/main/index.tsx
@@ -4,10 +4,10 @@ import LeftSide from "./LeftSide/page";
 const Main = () => {
   return (
     <div className="grid grid-cols-[1fr_3.5fr] container1 max-[1024px]:grid-cols-1">
-      <div className="flex flex-col gap-2 sticky top-0 max-[1024px]:hidden">
+      <div className="flex flex-col gap-2 z-10   max-[1024px]:hidden">
         <Aside />
       </div>
-      <div className=" flex-col gap-2 sticky top-0 max-[1024px]:flex hidden">
+      <div className=" flex-col gap-2  max-[1024px]:flex hidden">
         <Aside />
       </div>
       <div className="bg-[#f4f4f4]">
